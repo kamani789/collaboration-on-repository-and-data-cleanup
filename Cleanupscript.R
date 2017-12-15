5c6f81e1416fc8f5035eb6c1b5bfe86e114f25d0
U-AMERICAS\kzlc665 <kzlc665@USWRXL10E114AD6.astrazeneca.net>
2017-12-14 21:25:08 -0600

    Adding scriptfile

--- Cleanupscript.R
+++ Cleanupscript.R
@@ -1,4 +1,4 @@
-file_name_with_path <- "~/Desktop/MBA /Harrisburg/COURSE WORK/ANALY 510/Assignment7/collaboration-on-repository-and-data-cleanup/dirty_data.csv"
+file_name_with_path <- "C:/Users/kzlc665/Desktop/collaboration-on-repository-and-data-cleanup/dirty_data.csv"
 
 dirty_data <- read_csv(file_name_with_path)
 
@@ -19,11 +19,37 @@ b <- {}
 #should return 0L as there are no NAs
 x <- sum(is.na(dirty_data$Area))
 
-file_name_with_path_after_step1 <- "~/Desktop/MBA /Harrisburg/COURSE WORK/ANALY 510/Assignment7/collaboration-on-repository-and-data-cleanup/dirty_data_after_step1.csv"
+file_name_with_path_after_step1 <- "C:/Users/kzlc665/Desktop/collaboration-on-repository-and-data-cleanup/dirty_data_after_step1.csv"
 write.csv(dirty_data, file = file_name_with_path_after_step1, eol = "\n", na = "NA",
           row.names = FALSE, fileEncoding = "")
 
 
+##Step 2
+
+library('stringdist')
+library('stringi')
+library('stringr')
+
+#Remove all spaces and special characters
+dirty_data$Street <-  str_replace_all(dirty_data$Street, "[^[:alnum:]]", "")
+
+dirty_data$`Street 2` <-  str_replace_all(dirty_data$`Street 2`, "[^[:alnum:]]", "")
+
+#Capitalize first letter
+dirty_data$Street <- stri_trans_totitle(dirty_data$Street)
+dirty_data$`Street 2` <- stri_trans_totitle(dirty_data$`Street 2`)
+
+#Replace all occurances of streets with str. and avenues as ave.
+
+dirty_data$Street <-  gsub("treet", "tr.", dirty_data$Street)
+dirty_data$Street <-  gsub("venue", "ve.", dirty_data$Street)
+
+dirty_data$`Street 2` <-  gsub("treet", "tr.", dirty_data$`Street 2`)
+dirty_data$`Street 2` <-  gsub("venue", "ve.", dirty_data$`Street 2`)
+
+file_name_with_path_after_step2 <- "C:/Users/kzlc665/Desktop/collaboration-on-repository-and-data-cleanup/dirty_data_after_step2.csv"
+write.csv(dirty_data, file = file_name_with_path_after_step2, eol = "\n", na = "NA",
+          row.names = FALSE, fileEncoding = "")
 
 
 
